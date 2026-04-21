eceffb24ae6de9342be6d2665a23f77ce04feea9
arin kodolikar <arinkod28@gmail.com>
2026-04-21 17:18:24 -0400

    fix Express 5 catch-all route wildcard syntax

--- src/server/index.js
+++ src/server/index.js
@@ -42,7 +42,7 @@ app.use('/api/files', require('./routes/upload'));
 // Serve static frontend in production
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../../dist')));
-  app.get('*', (req, res) => {
+  app.use((req, res) => {
     res.sendFile(path.join(__dirname, '../../dist/index.html'));
   });
 }
